0f2620623b51cf8c5b76778e0167349e58361125
Miller <miller.98@hotmail.com>
2012-12-03 17:05:37 -0200

    Update Seg 03 Dez, 17:05

--- App/Controller/Page.php
+++ App/Controller/Page.php
@@ -14,10 +14,10 @@ class Page extends Action {
     //$page->insere(array("nome" => "123", "status" => "1", "url" => "/pagina/123/", "conteudo" => base64_encode("zsçodfgnaçflg</br> niqaç ogknaçlgnq akgban lkfjg nadlçf </br>gnsalfg naçlg naçsl dgna lksdgnaçlsd fnlaksjdfnlak sjdnflasn dlasdnf lasnd flasndlf</br> jasd nflaksdnf"), "titulo" => "TESTE"));
     $cursor = $page->busca($pagina);
     //$cursor1 = $page->buscaTodas();
-    if($cursor->count() < 1){
+    if ($cursor->count() < 1) {
       $this->view->paginaError = "<h1>Página não encontrada.</h1>";
       $this->view->pagina = null;
-    }else{
+    } else {
       $this->view->pagina = $page->busca($pagina);
     }
     $this->render();
@@ -27,9 +27,25 @@ class Page extends Action {
     if (isset($_POST['submit'])) {
       $page = new Pages();
       unset($_POST['submit']);
-      $page->insere(array("nome" => $_POST['nome'], "status" => $_POST['status'], "url" => $_POST['url'], "conteudo" => base64_encode($_POST['conteudo']), "titulo" => $_POST['titulo']));
+      $page->insere(array("nome" => $_POST['nome'], "status" => $_POST['status'], "url" => $_POST['url'], "conteudo" => $_POST['conteudo'], "titulo" => $_POST['titulo']));
     }
 
+    $this->render();
+  }
+
+  protected function testeAction() {
+    $this->render();
+  }
+
+  protected function cadastroAction() {
+    $this->render();
+  }
+
+  protected function contatoAction() {
+    $this->render();
+  }
+
+  protected function termosAction() {
     $this->render();
   }
 
--- public/js/default.js
+++ public/js/default.js
@@ -1,20 +1,14 @@
 $(document).ready(function(){
 
 });
-$(document).on("click", ".show-page-loading-msg", function() {
-  var $this = $( this ),
-  theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme,
-  msgText = $this.jqmData("msgtext") || $.mobile.loader.prototype.options.text,
-  textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
-  textonly = !!$this.jqmData("textonly");
-  html = $this.jqmData("html") || "";
-  $.mobile.loading( 'show', {
-    text: msgText,
-    textVisible: textVisible,
-    theme: theme,
-    textonly: textonly,
-    html: html
+$(document).on("click", "a", function() {
+  /* $.mobile.loading( 'show', {
+    text: 'foo',
+    textVisible: false,
+    theme: 'a',
+    html: ""
   });
+  */
 })
 .on("click", ".hide-page-loading-msg", function() {
   $.mobile.loading( 'hide' );
